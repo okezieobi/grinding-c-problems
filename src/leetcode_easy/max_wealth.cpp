f70cf53c97e4f82ebc1385ddf334fd3db0597b20
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-03-16 15:43:26 +0100

    doing

---
+++ src/leetcode_easy/max_wealth.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int m = INT_MIN;
+        for (int i = 0; i < accounts.size(); i++) {
+            int w = 0;
+            for (int j = 0; j < accounts.size(); j++) {
+                w += accounts[j];
+                if (w > m) {
+                    m = w;
+                }
+            }
+        }
+        return m;
+    }
+};
