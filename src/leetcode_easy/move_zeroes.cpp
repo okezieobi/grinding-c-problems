5b02fe3c6fe2d7c09741eb32c2fb72743b1d0a63
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-03-16 14:34:37 +0100

    done

--- src/leetcode_easy/move_zeroes.cpp
+++ src/leetcode_easy/move_zeroes.cpp
@@ -26,16 +26,32 @@ using namespace std;
 
 class Solution {
     public:
-      void moveZeroes(vector<int>& nums) {
-          vector<int> result;
-          for (int i = 0; i < nums.size(); i++) {
-              if (nums[i] != 0)
-                  result.push_back(nums[i]);
-          }
-          for (int i = 0; i < nums.size(); i++) {
-              if (nums[i] == 0)
-                  result.push_back(0);
-          }
-          nums = result;
-      }
+        void moveZeroesTwo(vector<int>& nums){
+            int nonZeroIndex = 0;
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] != 0) {
+                    nums[nonZeroIndex] = nums[i];
+                    nonZeroIndex++;
+                }
+            }
+            for (int i = nums.size() - 1; i >= nonZeroIndex; i--) {
+                nums[i] = 0;
+            }
+        };
+
+        void moveZeroes(vector<int> &nums)
+        {
+            vector<int> result;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (nums[i] != 0)
+                    result.push_back(nums[i]);
+            }
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (nums[i] == 0)
+                    result.push_back(0);
+            }
+            nums = result;
+        }
 };
